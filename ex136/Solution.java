78b9ba6162c64bb475a4b7b2aa286b92b9a144b5
KSha <kingsfish95@gmail.com>
2016-12-09 17:34:03 +0800

    change into another solution
    XOR method

--- ex136/Solution.java
+++ ex136/Solution.java
@@ -1,22 +1,11 @@
 package leetcode.ex136;
 
-import java.util.HashMap;
-
 public class Solution {
 	public int singleNumber(int[] nums) {
-		HashMap<Integer, Integer> table = new HashMap<>();
+		int result = 0;
 		for (int i = 0; i < nums.length; i++) {
-			if (table.get(nums[i]) != null) {
-				table.put(nums[i], table.get(nums[i]) + 1);
-			} else {
-				table.put(nums[i], 1);
-			}
+			result ^= nums[i];
 		}
-		for (int i : table.keySet()) {
-			if (table.get(i) == 1) {
-				return i;
-			}
-		}
-		return 0;
+		return result;
     }
 }
