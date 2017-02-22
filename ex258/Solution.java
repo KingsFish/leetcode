9423c2f6789ce1f85495058a64ba1fa531b8aa30
KSha <kingsfish95@gmail.com>
2017-02-22 11:37:31 +0800

    AC 258

---
+++ ex258/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.ex258;
+
+public class Solution {
+	public int addDigits(int num) {
+		int result = 0;
+        while (num >= 10) {
+        	while (num != 0) {
+				result += num % 10;
+				num /= 10;
+			}
+        	num = result;
+        	result = 0;
+        }
+        return num;
+    }
+}
