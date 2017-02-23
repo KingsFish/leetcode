b54f7a95971de14ad42564a7c4e4ec542e78b060
KSha <kingsfish95@gmail.com>
2017-02-23 10:55:02 +0800

    AC 415

---
+++ ex415/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.ex415;
+
+public class Solution {
+
+	public String addStrings(String num1, String num2) {
+		int i = num1.length() - 1, j = num2.length() - 1;
+		int x = 0, y = 0, carry = 0, temp = 0;
+		StringBuilder builder = new StringBuilder();
+		while (i >= 0 || j >= 0 || carry == 1) {
+			x = i < 0 ? 0 : num1.charAt(i) - '0';
+			y = j < 0 ? 0 : num2.charAt(j) - '0';
+			temp = x + y + carry;
+			carry = temp / 10;
+			builder.append(temp % 10);
+			i --;
+			j --;
+		}
+		return builder.reverse().toString();
+    }
+}
