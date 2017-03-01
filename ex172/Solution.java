cb25689b7b464aee41cffaf3fa9cbf206c84adcc
KSha <kingsfish95@gmail.com>
2017-03-01 22:00:34 +0800

    AC 172

---
+++ ex172/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.ex172;
+
+public class Solution {
+	public int trailingZeroes(int n) {
+        int i = 0;
+        while (n > 0) {
+            n /= 5;
+            i += n;
+        }
+        return i;
+    }
+}
