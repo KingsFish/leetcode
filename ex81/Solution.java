0a22e1ec17bd674d7a11782901cc28e5e6bf918c
KSha <kingsfish95@gmail.com>
2017-06-01 09:40:42 +0800

    AC 81

---
+++ ex81/Solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean search(int[] nums, int target) {
+		if (nums.length == 0) return false;
+		int lo = 0, hi = nums.length - 1;
+		while(lo < hi) {
+			int mid = lo + (hi - lo) / 2;
+			if (nums[mid] == target) {
+				return true;
+			}
+			if (nums[mid] > nums[hi]){
+				if (target < nums[mid] && nums[lo] <= target) hi = mid;
+				else lo = mid + 1;
+			} else if (nums[mid] < nums[hi]) {
+				if (target > nums[mid] && target <= nums[hi]) lo = mid + 1;
+				else hi = mid;
+			} else {
+				hi --;
+			}
+		}
+		return nums[lo] == target;
+	}
+}
