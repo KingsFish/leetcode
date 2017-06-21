69e5538cd0022e5c8f69988111945bf06c44f964
fish <kingsfish95@gmail.com>
2017-06-21 16:10:35 +0800

    AC 322

---
+++ ex322/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.ex322;
+
+public class Solution {
+
+	public int coinChange(int[] coins, int amount) {
+		int [] d= new int[amount + 1];
+		for (int i = 0; i < d.length; i++) {
+			d[i] = amount + 1;
+		}
+		d[0] = 0;
+        for (int i = 1; i < d.length; i++) {
+			for (int j = 0; j < coins.length; j++) {
+				if (coins[j] <= i && d[i - coins[j]] + 1 < d[i]) {
+					d[i] = d[i - coins[j]] + 1;
+				}
+			}
+		}
+        return d[amount] > amount ? -1 : d[amount];
+    }
+}
