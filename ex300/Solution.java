965a561e94004400fdb40f2d2115f67a6cff347b
Fish <kingsfish95@gmail.com>
2017-07-15 14:48:59 +0800

    AC 300

--- ex300/Solution.java
+++ ex300/Solution.java
@@ -5,13 +5,10 @@ import java.util.Arrays;
 public class Solution {
 
 	public int lengthOfLIS(int[] nums) {
-		if (nums.length == 0) {
-			return 0;
-		}
         int [] result = new int [nums.length];
-        Arrays.fill(result, 1);
-        int max = 1;
-        for (int i = 1; i < result.length; i++) {
+        int max = 0;
+        for (int i = 0; i < result.length; i++) {
+			result[i] = 1;
 			for (int j = 0; j < i; j++) {
 				if (nums[j] < nums[i]) {
 					result[i] = result[i] < result[j] + 1 ? result[j] + 1 : result[i];
