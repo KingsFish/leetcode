fed1f73e71753759458c8778865469116d36d3b5
Fish <kingsfish95@gmail.com>
2017-10-13 21:44:44 +0800

    AC 71

---
+++ ex71/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.ex71;
+
+public class Solution{
+    public String simplifyPath(String path) {
+        String [] strs = path.split("/");
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < strs.length; i ++){
+            if (strs[i].equals("..")){
+                if (stack.size() > 0){
+                    stack.pop();
+                }
+            } else if (strs[i].equals(".") || strs[i].length() == 0){
+                continue;
+            } else {
+                stack.push(strs[i]);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (stack.size() != 0){
+            sb.insert(0, stack.pop());
+            sb.insert(0, "/");
+        }
+        return sb.length() == 0 ? "/" : sb.toString();
+    }
+}
