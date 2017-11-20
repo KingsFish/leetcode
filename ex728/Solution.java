50a651108740fbea76c3d26c6d0219da38a2cf2f
Fish <kingsfish95@gmail.com>
2017-11-20 17:57:05 +0800

    AC 728

---
+++ ex728/Solution.java
@@ -0,0 +1,30 @@
+package leetcode.ex728;
+
+public class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> result = new ArrayList<>();
+        while (left < 10){
+            result.add(left);
+            left ++;
+        }
+        while(left <= right){
+            if (canDivide(left)){
+                result.add(left);
+            }
+            left ++;
+        }
+        return result;
+    }
+    
+    private boolean canDivide(int num){
+        int times = 10, num_bak = num;
+        while(num != 0){
+            int tmp = num % times;
+            if (tmp == 0 || num_bak % tmp != 0){
+                return false;
+            }
+            num /= times;
+        }
+        return true;
+    }
+}
