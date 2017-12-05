8b1a29e8293a806d9a039b6206d801e165ea1ce1
Fish <kingsfish95@gmail.com>
2017-12-05 22:01:34 +0800

    AC 605

---
+++ ex605/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.ex605;
+
+public class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int sum = 0;
+        for (int i = 0; i < flowerbed.length; i++){
+            if (flowerbed[i] == 0 
+                && (i == 0 || flowerbed[i - 1] == 0) 
+                && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)){
+                sum ++;
+                i ++;
+            }
+        }        
+        return sum >= n;
+    }
+}
